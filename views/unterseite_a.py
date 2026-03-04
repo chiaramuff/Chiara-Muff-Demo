c8fd144b184a10b18a84fd9c7c302b44bcc54ded
chiaramuff <muffchi1@students.zhaw.ch>
2026-03-04 14:14:50 +0100

    pH-Rechner Update 2.0

--- views/unterseite_a.py
+++ views/unterseite_a.py
@@ -3,20 +3,17 @@ from functions.ph_calculator import calculate_ph # Importiert deine Formel
 
 st.title("PH-Wert Rechner")
 
-st.write("Diese Seite ist eine Unterseite der Startseite.")
+st.write("Dies ist ein Rechner um den pH-Wert basierend auf der Konzentration von $H_3O^+$ zu berechnen. Gib die Konzentration in mol/L ein und klicke auf 'Berechnen'.")
 
 st.header("pH-Wert Rechner")
 st.write("Gib die $H_3O^+$ Konzentration ein, um den pH-Wert zu bestimmen.")
 
-# Formular wie in der Aufgabe gefordert
 with st.form("rechner_form"):
-    # Eingabefeld
     c = st.number_input("Konzentration (mol/L)", 
                             format="%.10f", 
                             value=0.001, 
                             step=0.0001)
     
-    # Bestätigungsbutton
     submitted = st.form_submit_button("Berechnen")
 
 if submitted:
@@ -25,7 +22,6 @@ if submitted:
     if res is not None:
         st.success(f"Der berechnete pH-Wert ist: *{res:.2f}*")
         
-        # Ein kleiner visueller Vergleich
         if res < 7:
             st.warning("Bereich: *Sauer* (z.B. Zitronensaft)")
         elif res > 7:
