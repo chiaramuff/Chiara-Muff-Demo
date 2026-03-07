bd539bd95ad95f624432048bc9281f53ccd7505c
Marisa Stöckli <marisa.stoeckli@gmx.ch>
2026-03-07 13:49:27 +0100

    updated

--- views/home.py
+++ views/home.py
@@ -6,7 +6,7 @@ st.title("pH-Wert Rechner")
 # !! WICHTIG: Eure Emails müssen in der App erscheinen!!
 
 """
-Dieser Rechner rechnet die Konzentrationen der Oxonium-Ionen ($H_3O^+$)** schnell und präzise in den entsprechenden **pH-Wert** um.
+Dieser Rechner rechnet die Konzentrationen der Oxonium-Ionen ($H_3O^+$)** schnell und präzise in den entsprechenden **pH-Wert** um und gibt an ob die Lösung sauer, basisch oder neutral ist.
 
 Diese App wurde von folgenden Personen entwickelt:
 - Marisa Stöckli (stoecma5@students.zhaw.ch)
