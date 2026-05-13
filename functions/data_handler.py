0edeb2dd70c778e98ff7423fd65e9abc19c6bafe
chiaramuff <muffchi1@students.zhaw.ch>
2026-05-13 17:58:32 +0200

    Improve data loading and saving logic in data_handler
    Enhance data loading checks and ensure DataFrame creation if data is None.

--- functions/data_handler.py
+++ functions/data_handler.py
@@ -2,12 +2,15 @@ import pandas as pd
 
 def load_tracker_data(dm, username):
     df = dm.load_app_data('allergie_daten.csv')
-    if not df.empty and "Nutzer" in df.columns:
+    if df is not None and not df.empty and "Nutzer" in df.columns:
         return df[df["Nutzer"] == username]
-    return df
+    return pd.DataFrame()
 
 def save_to_csv(dm, new_row_dict):
     df = dm.load_app_data('allergie_daten.csv')
+    if df is None:
+        df = pd.DataFrame()
+    
     df_new = pd.DataFrame([new_row_dict])
     df = pd.concat([df, df_new], ignore_index=True)
-    dm.save_app_data(df, 'allergie_daten.csv')
+    return dm.save_app_data(df, 'allergie_daten.csv')
