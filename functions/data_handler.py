0e51c2a168a001ff7984c5c675a3c62a70c3e405
chiaramuff <muffchi1@students.zhaw.ch>
2026-05-13 17:55:02 +0200

    Change data loading and saving functions to use app data

--- functions/data_handler.py
+++ functions/data_handler.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 def load_tracker_data(dm, username):
-    df = dm.load_user_data('allergie_daten.csv')
+    df = dm.load_app_data('allergie_daten.csv')
     if not df.empty and "Nutzer" in df.columns:
         return df[df["Nutzer"] == username]
     return df
 
 def save_to_csv(dm, new_row_dict):
-    df = dm.load_user_data('allergie_daten.csv')
+    df = dm.load_app_data('allergie_daten.csv')
     df_new = pd.DataFrame([new_row_dict])
     df = pd.concat([df, df_new], ignore_index=True)
-    dm.save_user_data(df, 'allergie_daten.csv')
+    dm.save_app_data(df, 'allergie_daten.csv')
